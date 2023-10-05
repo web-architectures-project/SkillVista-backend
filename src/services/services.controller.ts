44c6ab00abe2ddfe830fa22561f83b3435544236
Saul Burgess <Burgesssaul@gmail.com>
2023-10-05 13:21:41 +0100

    fix/minor fixes (#24)
    * Ignore language server of vscode
    
    * Solves "carage return" eslint error
    
    * Resolves several Eslint errors
    
    * Adds full project readme.md

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
