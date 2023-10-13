f43b986362365ae2796c649877d056a56ab5ce4e
Saul Burgess <Burgesssaul@gmail.com>
2023-10-13 12:18:58 +0100

    feature/reviews api and misc (#39)
    * Adjusts schema to fix bug on services
    
    * Modifes profile to fit Nest.js standards
    
    * Adds service_type API endpoint
    
    * Adds profiles api endpoint
    
    * Adds reviews api and tests
    
    * Adds eslint linting to the ci workflow

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,38 @@
-export class CreateReviewDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Length,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateReviewDto {
+  @ApiProperty({ required: true })
+  @IsInt()
+  @IsNotEmpty()
+  service_id: number;
+
+  @ApiProperty({ required: true })
+  @IsInt()
+  @IsNotEmpty()
+  reviewer_id: number;
+
+  @ApiProperty({ required: true })
+  @IsInt()
+  @IsNotEmpty()
+  @Length(1, 5)
+  rating: number;
+
+  @ApiProperty({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10000)
+  comment: string;
+
+  @ApiProperty({ required: true })
+  @IsDate()
+  @IsNotEmpty()
+  date_posted: Date;
+}
