09ad40152f597ac5a455c119f7abaf0b15474de6
Saul Burgess <Burgesssaul@gmail.com>
2023-10-20 12:42:57 +0100

    Fixes misc issues in reviews

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -8,9 +8,20 @@ export class ReviewsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createReviewDto: CreateReviewDto) {
-    return this.prisma.review.create({
-      data: createReviewDto,
-    });
+    try {
+      this.prisma.review.create({
+        data: createReviewDto,
+      });
+      return HttpStatus.CREATED;
+    } catch (error) {
+      if (
+        error.message.includes(
+          'Foreign key constraint failed on the field: `service_id`',
+        )
+      ) {
+        throw new NotFoundException('Service not found');
+      }
+    }
   }
 
   async findAll() {
@@ -18,22 +29,54 @@ export class ReviewsService {
   }
 
   async findOne(id: number) {
-    const review = await this.prisma.review.findUnique({
-      where: { review_id: id },
-    });
-    return review;
+    try {
+      const review = await this.prisma.review.findUnique({
+        where: { review_id: id },
+      });
+      return review;
+    } catch (error) {
+      if (
+        error.message.includes(
+          'Foreign key constraint failed on the field: `service_id`',
+        )
+      ) {
+        throw new NotFoundException('Service not found');
+      }
+    }
   }
 
   async update(id: number, updateReviewDto: UpdateReviewDto) {
-    return this.prisma.review.update({
-      where: { review_id: id },
-      data: updateReviewDto,
-    });
+    try {
+      this.prisma.review.update({
+        where: { review_id: id },
+        data: updateReviewDto,
+      });
+      return HttpStatus.OK;
+    } catch (error) {
+      if (
+        error.message.includes(
+          'Foreign key constraint failed on the field: `service_id`',
+        )
+      ) {
+        throw new NotFoundException('Service not found');
+      }
+    }
   }
 
   async remove(id: number) {
-    return this.prisma.review.delete({
-      where: { review_id: id },
-    });
+    try {
+      this.prisma.review.delete({
+        where: { review_id: id },
+      });
+      return HttpStatus.OK;
+    } catch (error) {
+      if (
+        error.message.includes(
+          'Foreign key constraint failed on the field: `service_id`',
+        )
+      ) {
+        throw new NotFoundException('Service not found');
+      }
+    }
   }
 }
