2e0429d01a8e9e9d9e4a5da6760f048ce365cf46
Kaustubh Trivedi <41857947+KaustubhTrivedi@users.noreply.github.com>
2023-10-22 16:53:06 +0100

    Feature/login api (#45)
    * Setup/Nestjs
    
    * Feature/implemented login logic
    
    * added comments to the code and added the .env file cause the tests were failing without that
    
    * wrote test to pass the failing test
    
    * refactored code
    
    * Working on user api - update and delete
    
    * Finished user api
    
    * Solved linting errors
    
    * Updates Eslint
    
    * Modifies returns for consistancy
    
    * Removes superflous spec
    
    * Modifies returns for consistancy
    
    * Fixes Eslint issue
    
    * Update now returns a status "ok"
    
    * Updates tests to accomadate changes
    
    ---------
    
    Co-authored-by: Solon <Solonerus@gmail.com>

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
+// import { CreateContactDto } from './dto/create-contact.dto';
+// import { UpdateContactDto } from './dto/update-contact.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ContactsService {
   constructor(private prisma: PrismaService) {}
 
-  create(createContactDto: CreateContactDto) {
+  create() {
     return 'This action adds a new contact';
   }
 
@@ -19,7 +19,7 @@ export class ContactsService {
     return `This action returns a #${id} contact`;
   }
 
-  update(id: number, updateContactDto: UpdateContactDto) {
+  update(id: number) {
     return `This action updates a #${id} contact`;
   }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,24 +2,38 @@ import {
   Controller,
   Get,
   Post,
-  Patch,
+  Put,
   Body,
   Param,
   Delete,
   HttpStatus,
   HttpException,
   Res,
+  HttpCode,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
+@UsePipes(new ValidationPipe())
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  // Register controllers
+
   @Post('register')
+  @ApiOperation({ summary: 'Create a new User' })
+  @ApiResponse({ status: 201, description: 'User create succesfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  @HttpCode(201)
   async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
     try {
       const user = await this.usersService.create(createUserDto);
@@ -30,14 +44,18 @@ export class UsersController {
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({ status: 201, description: 'User logged in succesfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async loginWithUsernameAndPassword(
     @Body() userData: CreateUserDto,
     @Res() res: Response,
   ) {
     try {
-      const token = await this.usersService.loginWithUsernameAndPassword(
-        userData,
-      );
+      const token =
+        await this.usersService.loginWithUsernameAndPassword(userData);
       return res.status(HttpStatus.OK).json({ token });
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.UNAUTHORIZED);
@@ -45,9 +63,14 @@ export class UsersController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Find all users' })
+  @ApiResponse({ status: 201, description: 'User data returned Successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'user not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async findAll(@Res() res: Response) {
     try {
-      const users = await this.usersService.findAll();
+      const users = this.usersService.findAll();
       return res.status(HttpStatus.OK).json(users);
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -55,6 +78,11 @@ export class UsersController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a single user' })
+  @ApiResponse({ status: 201, description: 'User returned successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async findOne(@Param('id') id: string, @Res() res: Response) {
     try {
       const user = await this.usersService.findOne(+id);
@@ -68,14 +96,19 @@ export class UsersController {
     }
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @ApiOperation({ summary: 'Update the user' })
+  @ApiResponse({ status: 201, description: 'User updated succesfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
     @Res() res: Response,
   ) {
     try {
-      const updatedUser = await this.usersService.update(+id, updateUserDto);
+      const updatedUser = this.usersService.update(+id, updateUserDto);
       return res.status(HttpStatus.OK).json(updatedUser);
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
@@ -83,9 +116,14 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user' })
+  @ApiResponse({ status: 201, description: 'User deleted successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   async remove(@Param('id') id: string, @Res() res: Response) {
     try {
-      await this.usersService.remove(+id);
+      this.usersService.remove(+id);
       return res.status(HttpStatus.NO_CONTENT).send();
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
