fcef99580605da363f3e43b57a02e958fac043f4
Solon <Solonerus@gmail.com>
2023-10-25 15:14:22 +0100

    Removes unecessary decorator

--- src/utils/decorators/local-only.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
-import { LocalRequestGuard } from './local-request.guard'; // You'll create this guard next
-
-export function LocalOnly() {
-  return applyDecorators(
-    SetMetadata('localOnly', true),
-    UseGuards(LocalRequestGuard),
-  );
-}
