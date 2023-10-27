8ced60182773205096553bda5086582fea7bae30
Saul Burgess <Burgesssaul@gmail.com>
2023-10-27 11:02:56 +0100

    Adjusts create-user to support profile gen

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,6 +6,10 @@ import {
   IsEmail,
   IsStrongPassword,
   IsEnum,
+  MaxLength,
+  IsOptional,
+  Length,
+  Matches,
 } from 'class-validator';
 export class CreateUserDto {
   @ApiProperty({ required: true })
@@ -29,4 +33,59 @@ export class CreateUserDto {
   @IsEnum(UserType, { message: 'Invalid User Type' })
   @IsNotEmpty({ message: 'First Name Required' })
   user_type: UserType;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  first_name: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  last_name: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @Matches(/^(\+\d{1,3}[- ]?)?\d{10}$/, {
+    message: 'Invalid phone number',
+  })
+  phone_number: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  address: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  city: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  county: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  @IsString()
+  @Length(7, 8)
+  Eircode: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  profile_picture_url?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  bio?: string;
 }
